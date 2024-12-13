b0b0464a14c7c53799898b69716eda2a6bf86969
Victor Jose <victordepena18@gmail.com>
2024-12-12 22:20:49 -0400

    Agregar archivos de proyecto.

---
+++ ProyectofFnal/Data/ProyectofFnalContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using BlazorWebAppMovies.Models;
+
+namespace ProyectofFnal.Data
+{
+    public class ProyectofFnalContext : DbContext
+    {
+        public ProyectofFnalContext (DbContextOptions<ProyectofFnalContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<BlazorWebAppMovies.Models.Movie> Movie { get; set; } = default!;
+    }
+}
